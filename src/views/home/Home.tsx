4baf79fdad77bcf6b457d20cff55d836cab18552
Chris <christopherc1331@gmail.com>
2023-10-08 21:40:37 -0500

    Fixed bug where hook was called from outside of solid component

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,9 +2,9 @@ import { useLocation } from "@solidjs/router";
 import { createSignal } from "solid-js";
 import { User } from "../../model/entity/User";
 
-const location = useLocation();
 
 const Home = () => {
+    const location = useLocation();
     const [user, setUser] = createSignal<User>(location.state as User);
     createSignal(() => {
         setUser(useLocation().state as User);
